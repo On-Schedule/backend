0af9f6f75e32aa2a10dfa4b5bb870c3b5c53d3ef
DougWelchons <doug.welchons@gmail.com>
2021-06-10 21:27:37 -0600

    Add unathorized error

--- spec/requests/get_user_endpoint_spec.rb
+++ spec/requests/get_user_endpoint_spec.rb
@@ -33,4 +33,84 @@ RSpec.describe "GET user endpoint" do
       expect(body[:data][:attributes][:api_key]).to eq(user.api_key)
     end
   end
+
+  describe "Sad path and Edge case" do
+    it "returns an unauthorized error if a user doesn't exist" do
+      get "/api/v1/users/1?api_key=123456789"
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401) #propper code would be 404 but want to match bad api_key error
+      expect(body).to be_a(Hash)
+      expect(body.keys).to eq([:error])
+      expect(body[:error]).to eq("unauthorized")
+    end
+
+    it "returns an unauthorized error if the api_key and user id dont match" do
+      company = Company.create!(name: "Rowan Electric")
+
+      user = User.create!( company_id: company.id,
+                    first_name: 'Doug',
+                    last_name: 'Welchons',
+                    email: "email@domain.com",
+                    password: "another_password",
+                    password_confirmation: "another_password",
+                    api_key: "key" )
+
+      get "/api/v1/users/#{user.id}?api_key=not_correct_key"
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401)
+      expect(body).to be_a(Hash)
+      expect(body.keys).to eq([:error])
+      expect(body[:error]).to eq("unauthorized")
+    end
+
+    it "returns an unauthorized error if no api_key is provided" do
+      company = Company.create!(name: "Rowan Electric")
+
+      user = User.create!( company_id: company.id,
+                    first_name: 'Doug',
+                    last_name: 'Welchons',
+                    email: "email@domain.com",
+                    password: "another_password",
+                    password_confirmation: "another_password",
+                    api_key: "key" )
+
+      get "/api/v1/users/#{user.id}"
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401)
+      expect(body).to be_a(Hash)
+      expect(body.keys).to eq([:error])
+      expect(body[:error]).to eq("unauthorized")
+    end
+
+    it "returns an unauthorized error if api_key is blank" do
+      company = Company.create!(name: "Rowan Electric")
+
+      user = User.create!( company_id: company.id,
+                    first_name: 'Doug',
+                    last_name: 'Welchons',
+                    email: "email@domain.com",
+                    password: "another_password",
+                    password_confirmation: "another_password",
+                    api_key: "key" )
+
+      get "/api/v1/users/#{user.id}?api_key="
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401)
+      expect(body).to be_a(Hash)
+      expect(body.keys).to eq([:error])
+      expect(body[:error]).to eq("unauthorized")
+    end
+  end
 end
