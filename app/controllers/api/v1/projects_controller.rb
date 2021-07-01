deb003e6da4279b5e7faab44cdea173aad12ec09
DougWelchons <doug.welchons@gmail.com>
2021-07-01 17:02:06 -0600

    Add changes to controllers

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::ProjectsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     if valid_user?(user)
+      return days_required unless valid_days?(params[:days_of_week])
       project = Project.new(project_params)
       project.company_id = user.company_id
       if project.save!
@@ -11,6 +12,8 @@ class Api::V1::ProjectsController < ApplicationController
         end
         render json: ProjectSerializer.new(project), status: :created
       end
+    else
+      unauthorized_request
     end
   end
 
@@ -21,5 +24,17 @@ class Api::V1::ProjectsController < ApplicationController
 
   def valid_user?(user)
     user.api_key == params[:api_key]
+  end
+
+  def valid_days?(days_of_week)
+    return false unless days_of_week
+    return false unless days_of_week.class == Array
+    return false unless days_of_week.count > 0
+
+    days_of_week.all? do |day|
+      DayOfWeek.all.any? do |week_day|
+        week_day.day == day
+      end
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  include ErrorHandling
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+  rescue_from ActiveRecord::RecordNotFound, with: :unauthorized_request
 end
